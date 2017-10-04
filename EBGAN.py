edfb5d30bbbce117429095519917d8b1d6494d5d
TwistedW <2359375192@qq.com>
2017-10-04 19:49:27 +0800

    updating

--- EBGAN.py
+++ EBGAN.py
@@ -196,8 +196,9 @@ class EBGAN(object):
 
                 # display training status
                 counter += 1
-                print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
-                      % (epoch, idx, self.num_batches, time.time() - start_time, d_loss, g_loss))
+                if idx % 200 == 0:
+                    print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
+                          % (epoch, idx, self.num_batches, time.time() - start_time, d_loss, g_loss))
 
                 # save training results for every 300 steps
                 if np.mod(counter, 300) == 0:
