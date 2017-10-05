dec714cfd2a2735302d2696a36d8a79ce714ab46
TwistedW <2359375192@qq.com>
2017-10-05 21:10:11 +0800

    updating

--- BEGAN.py
+++ BEGAN.py
@@ -207,7 +207,8 @@ class BEGAN(object):
                     manifold_h = int(np.floor(np.sqrt(tot_num_samples)))
                     manifold_w = int(np.floor(np.sqrt(tot_num_samples)))
                     save_images(samples[:manifold_h * manifold_w, :, :, :], [manifold_h, manifold_w],
-                                './' + check_folder(self.result_dir + '/' + self.model_dir) + '/' + self.model_name + '_train_{:02d}_{:04d}.png'.format(
+                                './' + check_folder(self.result_dir + '/' + self.model_dir) + '/' + self.model_name +
+                                '_train_{:02d}_{:04d}.png'.format(
                                     epoch, idx))
 
             # After an epoch, start_batch_id is set to zero
