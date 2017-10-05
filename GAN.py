07c2e5ed2fef46fa38ee3932142a09d434c031f1
TwistedW <2359375192@qq.com>
2017-10-05 21:18:11 +0800

    updating

--- GAN.py
+++ GAN.py
@@ -177,7 +177,8 @@ class GAN(object):
                 self.writer.add_summary(summary_str, counter)
 
                 # update G network
-                _, summary_str, g_loss = self.sess.run([self.g_optim, self.g_sum, self.g_loss], feed_dict={self.z: batch_z})
+                _, summary_str, g_loss = self.sess.run([self.g_optim, self.g_sum, self.g_loss],
+                                                       feed_dict={self.z: batch_z})
                 self.writer.add_summary(summary_str, counter)
 
                 # display training status
